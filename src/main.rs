416a0d8115d05217c6e5b192b2af383478a99fbc
Felix Kahle <felix.kahle21@gmail.com>
2024-10-24 23:53:04 +0200

    pivot

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,7 @@
 // Copyright 2024 Felix Kahle. All rights reserved.
 
-use nalgebra::DMatrix;
-use crate::tableau::Tableau;
-
 mod tableau;
 
 fn main() {
-    let matrix = DMatrix::from_row_slice(3, 3, &[9.0, 3.0, 27.0, 2.0, 1.0, 7.0, 2.0, 2.0, 12.0]);
-    let row_names = vec!["row1".to_string(), "pooopo".to_string(), "row3".to_string()];
-    let column_names = vec!["x1".to_string(), "x2".to_string(), "RHS".to_string()];
-
-    let tableau = Tableau::new(matrix, row_names, column_names);
-
-    // Print the tableau using the Display trait
-    println!("{}", tableau);
+    println!("Hello, world!");
 }
--- src/tableau.rs
+++ src/tableau.rs
@@ -1,10 +1,11 @@
 // Copyright 2024 Felix Kahle. All rights reserved.
 
+#![allow(dead_code)]
+
 use std::fmt::{Display};
 
-use nalgebra::DMatrix;
+use nalgebra::{DMatrix, Dyn, MatrixView, U1};
 use tabled::settings::Style;
-use tabled::Table;
 
 /// A tableau that represents a linear program.
 pub struct Tableau {
@@ -110,16 +111,101 @@ impl Tableau {
     pub fn get_objective_value(&self) -> f64 {
         self.matrix[(self.rows() - 1, self.cols() - 1)]
     }
+    
+    /// Get the rhs vector of the tableau.
+    ///
+    ///
+    /// # Returns
+    /// The rhs vector of the tableau.
+    /// 
+    /// # Note
+    /// The rhs vector is the last column of the matrix without the last row.
+    pub fn rhs_vector(&self) -> MatrixView<f64, Dyn, Dyn, U1, Dyn> {
+        // The rhs vector is the last column of the matrix without the last row.
+        self.matrix.view((0, self.cols() - 1), (self.rows() - 1, 1))
+    }
+    
+    /// Get the objective coefficients of the tableau.
+    /// 
+    /// # Returns
+    /// The objective coefficients of the tableau.
+    /// 
+    /// # Note
+    /// The objective coefficients are the last row of the matrix without the last column.
+    pub fn objective_coefficients(&self) -> MatrixView<f64, Dyn, Dyn, U1, Dyn> {
+        // The objective coefficients are the last row of the matrix without the last column.
+        self.matrix.view((self.rows() - 1, 0), (1, self.cols() - 1))
+    }
+    
+    ///Check if the current tableau is feasible.
+    ///
+    /// # Returns
+    /// - `true` if the tableau is feasible.
+    /// - `false` if the tableau is not feasible.
+    pub fn is_feasible(&self) -> bool {
+        let rhs_vector = self.rhs_vector();
+        
+        // Check if all values of the rhs vector are greater or equal to zero.
+        rhs_vector.iter().all(|value| *value >= 0.0)
+    }
+    
+    /// Check if the tableau is optimal.
+    ///
+    /// # Returns
+    /// - `true` if the tableau is optimal.
+    /// - `false` if the tableau is not optimal.
+    pub fn is_optimal(&self) -> bool {
+        let objective_coefficients = self.objective_coefficients();
+        
+        // Check if all values of the objective are greater or equal to zero.
+        objective_coefficients.iter().all(|value| *value >= 0.0)
+    }
+
+    /// Perform a pivot operation on the tableau.
+    ///
+    /// # Arguments
+    /// * `pivot_row` - The index of the pivot row.
+    /// * `pivot_column` - The index of the pivot column.
+    ///
+    /// # Note
+    /// The pivot operation is performed in place using the gaussian elimination method.
+    pub fn gaussian_pivot(&mut self, pivot_row: usize, pivot_column: usize) {
+        // Get the pivot element.
+        let pivot_element = self.matrix[(pivot_row, pivot_column)];
+        
+        let mut pivot_row_mut = self.matrix.row_mut(pivot_row);
+        pivot_row_mut.scale_mut(1.0 / pivot_element);
+        let pivot_row_copy = self.matrix.row(pivot_row).clone_owned();
+
+        // Perform row operations to eliminate other entries in the pivot column.
+        let num_rows = self.rows();
+        for r in 0..num_rows {
+            if r != pivot_row {
+                let factor = self.matrix[(r, pivot_column)];
+                let mut current_row_mut = self.matrix.row_mut(r);
+                current_row_mut -= factor * &pivot_row_copy;
+            }
+        }
+    }
 }
 
+/// Implement the Display trait for Tableau.
+/// 
+/// # Note
+/// This implementation uses the tabled crate to display the tableau
+/// in a well formatted table.
 impl Display for Tableau {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         let mut builder = tabled::builder::Builder::default();
         
+        // Push the column headers to the table.
+        // The first cell (0, 0) is empty, because this column is used for the row names.
         let mut column_header = vec![String::new()];
         column_header.extend(self.column_names.iter().cloned());
         builder.push_record(column_header);
         
+        // Push the rows to the table.
+        // The first cell of each row is the row name.
         for (i, row) in self.matrix.row_iter().enumerate() {
             let mut row_data = Vec::new();
             row_data.push(self.row_names[i].to_string());
@@ -128,12 +214,15 @@ impl Display for Tableau {
             builder.push_record(row_data);
         }
 
+        // The first column is the index column.
         let mut table = builder
             .index()
             .column(0)
             .name(None)
             .build();
 
+        // Set the style of the table to markdown,
+        // and write the table to the formatter.
         table.with(Style::markdown());
         write!(f, "{}", table)
     }
